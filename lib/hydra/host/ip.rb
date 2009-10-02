9cdcbfe81b06991fb54a67553db81cf4176c75ac
Kyle Kingsbury <kyle@vodpod.com>
2009-10-02 11:52:34 -0700

    Sketched out some IP address host methods...

---
+++ lib/hydra/host/ip.rb
@@ -0,0 +1,19 @@
+module Hydra::Host::IP
+  # Host methods for IP addresses
+  
+  # Get all active IP addresses.
+  def ips
+    `ifconfig -a`.split("\n\n").map { |stanza|
+      stanza.split("\n").find { |line|
+        line =~ /inet addr:\s*([\d\.]{7..15})/
+        $1
+      }
+    }.compact
+  end
+
+  def private_ips
+  end
+
+  def public_ips
+  end
+end
